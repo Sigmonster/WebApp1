7510cc624c5210e5716413fc97c3852bd41b0b0f
Sigmonster <alexandersigmon@gmail.com>
2019-03-14 01:14:15 -0400

    initial commit

---
+++ WebApplication1/Models/ErrorViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace WebApplication1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+
+
+}
